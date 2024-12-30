f1a1eacb64c19b55ec1a6d459ab40e0891cdb609
R Mahesh Babu <rmaheshbabu9972@gmail.com>
2024-12-30 12:26:30 +0530

    Variables.sh

---
+++ Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#script to show how to use variables
+reg=52
+name="Mahesh"
+age=21
+echo "My name is : $name having Register number : $reg and my age is : $age"
+name="Babu"
+echo "My name is : $name"
